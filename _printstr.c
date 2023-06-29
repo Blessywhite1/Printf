7c79654001b79177b536ddedfbb8acff7aad3f6e
Blessywhite1 <chiwendub1@gmail.com>
2023-06-29 14:05:07 -0700

    main

---
+++ _printstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _printstring: function that print a string
+ * @s: value of strings
+ *  Return [0]
+ */
+int main (void)
+{
+	char str[5] = "study";
+
+	printf("string: ");
+	printf("\"%s\"", str);
+
+	return 0;
+}
